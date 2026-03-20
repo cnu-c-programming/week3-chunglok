6839331c8d7e5103f5376885ecccc83ef4fd731c
최준화 <choijunhwa072@gmail.com>
2026-03-20 12:21:03 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdarg.h>
 
+int average(int count, ...){
+    va_list ap;
+    va_start(ap, count);
+    int mean = 0;
+    for(int i = 0; i < count; i++)
+        mean += va_arg(ap, int);
+    va_end(ap);
+    return mean/count;
+}
 int main() {
-
+    printf("%d\n", average(3, 10, 20, 30));
+    printf("%d\n", average(5, 10, 20, 30, 40, 50));
     return 0;
 }
