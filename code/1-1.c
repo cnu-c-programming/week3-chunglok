dee3bf085bae41577d2b58132d74ac222fefe437
최준화 <choijunhwa072@gmail.com>
2026-03-20 11:29:10 +0900

    Implement basic arithmetic functions
    Added basic arithmetic functions for addition, subtraction, multiplication, and division.

--- code/1-1.c
+++ code/1-1.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
-
+int add(int a, int b){
+    return a + b;
+}
+int sub(int a, int b){
+    return a - b;
+}
+int mul(int a, int b){
+    return a * b;
+}
+int div(int a, int b){
+    return a / b;
+}
 int main() {
-
+    int a = 10, b = 5;
+     printf("%d\n", add);
+    printf("%d\n", sub);
+    printf("%d\n", mul);
+    printf("%d\n", div);
     return 0;
 }
