3b0dc54a568626c8e8ba8667ec1f89bd4e31252c
최준화 <choijunhwa072@gmail.com>
2026-03-20 12:36:45 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+void my_sum(char type, int count, ...){
+    va_list(ap);
+    va_start(ap, type);
+    va_end(ap);
+    return 0;
+}
 int main() {
-
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C', 'N', 'U');
+    my_sum('D', 4, 10, 20, 30, 40);
     return 0;
 }
