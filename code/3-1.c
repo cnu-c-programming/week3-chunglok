1c3accd2a9bbae22b188bcd6d9d66d44fb3e6e24
최준화 <choijunhwa072@gmail.com>
2026-03-20 12:04:11 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
+int call_count(){
+    
+    return ++count;
+}
 int main() {
-
+    printf("%d\n", call_count());
+    printf("%d\n", call_count());
+    printf("%d\n", call_count());
     return 0;
 }
