a3a06e0d2c83f5831c497f4cec6232a54c13facd
최준화 <choijunhwa072@gmail.com>
2026-03-26 11:03:33 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 void swap(int *x, int *y){
-    x ^ y;
-    return x, y;
+    int p = *x;
+    *x = *y;
+    *y = p;
+    return 0;
 }
 int main() {
     int x = 100, y = 200;
-    swap(x, y);
+    printf("x = %d, y = %d\n", x, y);
+    swap(&x, &y);
     printf("x = %d, y = %d\n", x, y);
     
     return 0;
