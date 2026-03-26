d5898807f074518ee79eb9a96fc587e93a75bfc3
최준화 <choijunhwa072@gmail.com>
2026-03-26 17:32:25 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -6,7 +6,10 @@ void my_sum(char type, int count, ...){
     va_start(ap, count);
      if(type == 'S'){
         for(int i = 0; i < count; i++){
-            printf("%s ", va_arg(ap, char*));
+            printf("%s", va_arg(ap, char*));
+            for(int j = 0; j < count - 1 ; j++){
+                printf(" ");
+            }
         }
         printf("\n");
     }
